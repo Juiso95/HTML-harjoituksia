e20198e1b712004248f795d70bb4a0aaed0b3c24
Juiso95 <juuso95juuso@hotmail.com>
2024-09-23 14:29:54 +0300

    functiot

--- JavaScript/Functioharjoituksia.js
+++ JavaScript/Functioharjoituksia.js
@@ -89,9 +89,89 @@ function laskePienin(taulukkoPienin) {
     return Math.min(...taulukkoPienin);
 }
 console.log(laskePienin(([2,7,1,9,3])));
+console.log("..............................................................................................................................................");
 
 // Laske Taulukon Summa
 console.log("F10");
 function laskeTaulukonSumma(taulukkoSumma) {
-    
+    var summa = 0;
+    for (var i = 0; i < taulukkoSumma.length; i++)
+{
+    summa += taulukkoSumma[i];
 }
+return summa;
+}
+console.log(laskeTaulukonSumma([1,2,3,4,5]))
+console.log("..............................................................................................................................................");
+
+// Laske Potenssi
+console.log("F11");
+function laskePotenssi(kantaluku,eksponentti) {
+    return Math.pow(kantaluku,eksponentti);
+}
+console.log(laskePotenssi(4,2));
+console.log("..............................................................................................................................................");
+
+// Onko alkuluku?
+console.log("F12");
+function onkoAlkuluku(alkuluku) {
+    if (alkuluku < 2) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(alkuluku); i++) {
+        if (alkuluku % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(onkoAlkuluku(13));
+console.log(onkoAlkuluku(15));
+console.log(onkoAlkuluku(19));
+
+// Esimerkki vaihtoehto
+console.log("F12.1");
+function onkoAlkuluku(alkuluku) {
+    if (alkuluku <= 1) return false;
+    if (alkuluku <= 3) return true;
+    if (alkuluku % 2 === 0 || alkuluku % 3 === 0) return false;
+    let i = 5;
+    while ( i * i <= alkuluku) {
+        if (alkuluku % i === 0 || alkuluku % (i+2) === 0) return false;
+    }
+    return true;
+}
+console.log(onkoAlkuluku(13));
+console.log(onkoAlkuluku(15));
+console.log(onkoAlkuluku(19));
+console.log("..............................................................................................................................................");
+
+// Käänteinen parametri
+console.log("F13");
+function kaannaMerkkijono(kaanna) {
+    return kaanna.split('').reverse().join('');
+}
+console.log(kaannaMerkkijono("Morjensta"));
+console.log("..............................................................................................................................................");
+
+// Onko Palindromi
+console.log("F14");
+function onkoPalindromi(sana) {
+const kaannettySana = sana.split('').reverse().join('');
+    return sana === kaannettySana;
+}
+console.log(onkoPalindromi("anna"));
+console.log(onkoPalindromi("sanna"));
+console.log(onkoPalindromi("matti"));
+console.log(onkoPalindromi("eve"));
+
+// Onko Anagrammi
+console.log("F15");
+function onkoAnagrammi(sana11,sana22) {
+    const sana1Jarjestetty = sana11.split('').sort().join('');
+    const sana2Jarjestetty = sana22.split('').sort().join('');
+    return sana1Jarjestetty === sana2Jarjestetty;
+}
+console.log(onkoAnagrammi("kissa", "kassi"));
+console.log(onkoAnagrammi("kairo", "koira"));
+console.log(onkoAnagrammi("hevonen", "hamsteri"));
